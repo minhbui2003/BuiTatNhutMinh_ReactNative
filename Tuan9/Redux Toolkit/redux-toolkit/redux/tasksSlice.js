f0a5730602525996f7b043857b3408d9d4e91265
Bùi Tất Nhựt Minh <minhbui88888888@gmail.com>
2024-10-30 11:49:01 +0700

    Redux Toolkit

---
+++ Tuan9/Redux Toolkit/redux-toolkit/redux/tasksSlice.js
@@ -0,0 +1,14 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const tasksSlice = createSlice({
+  name: 'tasks',
+  initialState: [],
+  reducers: {
+    addTask: (state, action) => {
+      state.push(action.payload);
+    },
+  },
+});
+
+export const { addTask } = tasksSlice.actions;
+export default tasksSlice.reducer;
